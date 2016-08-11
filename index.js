7b7504b00b1f6ae7d3139c3174ac8aa6cc297b8b
Jack Bass <jbass2000@gmail.com>
2016-08-10 18:06:07 -0700

    fixed jscolor module

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var http	   = require('http'),
 
 
 
-const version = "0.1.32"
+const version = "0.1.33"
 
 var app = express();
 
@@ -15,7 +15,23 @@ var io = socketio(server);
 
 app.set('view engine', 'ejs');
 
-app.use('/lib', express.static(__dirname + '/lib'));
+app.use('/lib', express.static(__dirname + '/static'));
+
+app.get('/jscolor', (req, res) => {
+	res.sendFile(__dirname + '/lib/jscolor/jscolor.min.js');
+});
+
+app.get('/jquery', (req, res) => {
+	res.sendFile(__dirname + '/lib/jquery/dist/jquery.min.js');
+});
+
+app.get('/bootstrapjs', (req, res) => {
+	res.sendFile(__dirname + '/lib/bootstrap/dist/js/bootstrap.min.js');
+});
+
+app.get('/bootstrapcss', (req, res) => {
+	res.sendFile(__dirname + '/lib/bootstrap/dist/css/bootstrap.min.css');
+});
 
 app.get('/', (req, res) => {
 	res.render('index', {version: version});
